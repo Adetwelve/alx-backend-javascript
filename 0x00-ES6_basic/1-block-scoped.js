86c55f0a51faafe7ada3bdcce70416375c87ddfd
Adetwelve <niyero10@gmail.com>
2023-06-26 23:10:04 +0000

    fix

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,8 +1,10 @@
 export default function taskBlock(trueOrFalse) {
+  // set scope of task
   let task = false;
   let task2 = true;
 
   if (trueOrFalse) {
+  // set block scope 
     const task = true;
     const task2 = false;
   }
