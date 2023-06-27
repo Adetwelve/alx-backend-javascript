1866ada06173cdc14f7415c1a83265a13906cac6
Adetwelve <niyero10@gmail.com>
2023-06-27 21:00:02 +0000

    task10 for-of

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newA = [];
+
+  for (const value of array) {
+    newA.push(appendString + value);
+  }
+
+  return newA;
+}
