3fb7a75129fa4ee70a8cdd67e8468a45e65535d2
Adetwelve <niyero10@gmail.com>
2023-06-28 03:09:22 +0000

    fix

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,9 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      const response = {
+      resolve({
         status: 200,
         body: 'Success',
-      };
-      resolve(response);
       });
     } else {
       reject(new Error('The fake API is not working currently'));
