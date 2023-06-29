0d624c5cce8d0cc757bfd704a090e46bb025c1f0
Adetwelve <niyero10@gmail.com>
2023-06-29 18:18:48 +0000

    fix

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,9 +1,11 @@
 /* eslint-disable no-underscore-dangle */
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') throw new TypeError('Name must be a string');
-    if (typeof length !== 'number') throw new TypeError('Length must be a number');
-    if (!Array.isArray(students) || !students.every((element) => typeof element === 'string')) throw new TypeError('Student must be an array of string');
+    if (typeof name !== 'string') { throw new TypeError('Name must be a string'); }
+    if (typeof length !== 'number') { throw new TypeError('Length must be a number'); }
+    if (!Array.isArray(students) || !students.every((element) => typeof element === 'string')) {
+      throw new TypeError('Student must be an array of string');
+    }
 
     this._name = name;
     this._length = length;
