e3df76e3b42686658b862c09caeb50a48153a136
Adetwelve <niyero10@gmail.com>
2023-06-30 00:50:10 +0000

    task 2: implement a class and use setter and getters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -25,14 +25,23 @@ export default class HolbertonCourse {
   }
 
   set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
     this._name = name;
   }
 
   set length(length) {
+    if (typeof length !== 'string') {
+      throw new TypeError('Length must be a number');
+    }
     this._length = length;
   }
 
   set students(students) {
+    if (typeof students !== 'string') {
+      throw new TypeError('Student must be an array of string');
+    }
     this._students = students;
   }
 }
