da0af2335a049640c0ca2b5bd2219dbf4fe6ba73
Adetwelve <niyero10@gmail.com>
2023-07-02 16:49:09 +0000

    task 8: cast class into given type

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-underscore-dangle */
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof (size) === 'number') this._size = size;
+    if (typeof (location) === 'string') this._location = location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+}
