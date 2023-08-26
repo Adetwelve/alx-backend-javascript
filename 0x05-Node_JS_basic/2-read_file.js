24d8dfb9eb101e7c65016ead5b0874248c330474
Adetwelve <niyero10@gmail.com>
2023-08-26 02:55:08 +0000

    Reading a file synchronously and asynchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,29 +1,46 @@
 const fs = require('fs');
 
-const path = '/home/vagrant/alx-backend-javascript/0x05-Node_JS_basic/database.csv'
-function countStudents(path) {
-  try {
-    const data = fs.readFileSync(path, 'utf8');
-    const lines = data.split('\n').filter(line => line.trim() !== '');
-
-  if (lines.length === 0) {
+/**
+ * Counts the students in a CSV data file.
+ * @param {String} dataPath The path to the CSV data file.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
+const countStudents = (dataPath) => {
+  if (!fs.existsSync(dataPath)) {
     throw new Error('Cannot load the database');
   }
-
-  console.log(`Number of students: ${lines.length - 1}`);
-
-  const headings = lines[0].split(',');
-
-  headings.forEach((heading, index) => {
-    if (index !== 0) {
-      const students = lines.slice(1).map(line => line.split(',')[index]);
-      console.log(`Number of students in ${heading}: ${students.length}. List: ${students.join(', ')}`);
-      }
-    });
-
-  } catch (error) {
-    console.error(error.message);
+  if (!fs.statSync(dataPath).isFile()) {
+    throw new Error('Cannot load the database');
   }
-}
+  const fileLines = fs
+    .readFileSync(dataPath, 'utf-8')
+    .toString('utf-8')
+    .trim()
+    .split('\n');
+  const studentGroups = {};
+  const dbFieldNames = fileLines[0].split(',');
+  const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
 
-module.exports = countStudents
+  for (const line of fileLines.slice(1)) {
+    const studentRecord = line.split(',');
+    const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
+    const field = studentRecord[studentRecord.length - 1];
+    if (!Object.keys(studentGroups).includes(field)) {
+      studentGroups[field] = [];
+    }
+    const studentEntries = studentPropNames
+      .map((propName, idx) => [propName, studentPropValues[idx]]);
+    studentGroups[field].push(Object.fromEntries(studentEntries));
+  }
+
+  const totalStudents = Object
+    .values(studentGroups)
+    .reduce((pre, cur) => (pre || []).length + cur.length);
+  console.log(`Number of students: ${totalStudents}`);
+  for (const [field, group] of Object.entries(studentGroups)) {
+    const studentNames = group.map((student) => student.firstname).join(', ');
+    console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
+  }
+};
+
+module.exports = countStudents;
