d23a9d8b6c38b05cf66ef2cd182eea3217bf3f33
Adetwelve <niyero10@gmail.com>
2023-08-27 21:47:33 +0000

    task 3: using sinon spy a method

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,14 @@
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('sendPaymentRequestToApi', function() {
+  it('calls Utils.calculateNumber with correct arguments', function() {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20);
+    expect(spy.calledOnce).to.be.true;
+   // expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+  });
+});
---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,19 @@
+const Utils = { 
+  calculateNumber: function(type, a, b) {
+  const resultA = Math.round(a);
+  const resultB = Math.round(b);
+
+  if (type === 'SUM') {
+    return resultA + resultB;
+  } else if (type === 'SUBTRACT') {
+    return resultA - resultB;
+  } else if (type === 'DIVIDE') {
+    if (resultB === 0) {
+      return 'Error';
+    }
+    return resultA / resultB;
+  }
+ }
+}
+
+module.exports = Utils
